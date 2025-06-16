375daf40672dd18b89606c1fdbb2ca907fd675d3
Bishan Bhandari <108720684+bishanbhandari@users.noreply.github.com>
2025-06-16 13:00:32 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
-import Header from './Components/Header/Header';
+import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
+import Title from './Components/Title/Title';
+
 const App = () => {
   return (
     <>
     <Navbar/>
-    <Header/>
-    <Programs/>
+    <Hero/>
+    <div className="container">
+      <Title subtitile="Our Program" title="What We Offer"/>
+      <Programs/>  
+    </div>
     </>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import './Header.css';
+import './Hero.css';
 import dark_arrow from '../../assets/dark-arrow.png';
 
-function Header() {
+function Hero() {
   return (
-    <div className='header'>
-        <div className="header-text">
+    <div className='hero'>
+        <div className="hero-text">
             <h1>Excellence in Education provides , Strength for the Future</h1>
             <p>Our curriculum is designed to foster academic excellence, 
                 practical skills, and critical thinking across a wide 
                 range of disciplines.</p>
-
                 <button className='btn'>Explore <img src={dark_arrow} alt="" /> </button>
         </div>
     </div>
   )
 }
 
-export default Header;
+export default Hero;
